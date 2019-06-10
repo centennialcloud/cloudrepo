113f945320b5bfac5e10e698694a8c539ba815e8
Kent Tokunaga <KentTokunaga@gmail.com>
2019-06-10 14:10:22 -0500

    Create network interface

--- main.tf
+++ main.tf
@@ -33,3 +33,17 @@ resource "azurerm_subnet" "internal" {
   virtual_network_name = "${azurerm_virtual_network.main.name}"
   address_prefix       = "10.0.0.0/24"
 }
+
+# Create NIC
+# TODO: Make NIC without hardcoding it
+resource "azurerm_network_interface" "main" {
+  name                = "vm-nic"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = "${azurerm_subnet.internal.id}"
+    private_ip_address_allocation = "Dynamic"
+  }
+}
