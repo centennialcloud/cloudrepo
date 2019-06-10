f36384f43edfc19f139053abcca24312e5b056bd
Kent Tokunaga <KentTokunaga@gmail.com>
2019-06-10 14:08:31 -0500

    Update variables

--- az-variables.tf
+++ az-variables.tf
@@ -1,15 +1,25 @@
-
 variable "vm-size" {
   type    = "list"
-  default = ["Standard_B1s", "Standard_B2s", "Standard_B4ms"] 
+  default = ["Standard_B1s", "Standard_B2s", "Standard_B4ms"]
 }
 
 variable "vm-image" {
-    type = "list"
-    default = ["UbuntuServer:16.04-LTS:16.04.201905303", "UbuntuLTS"]
+  type    = "list"
+  default = ["UbuntuServer:16.04-LTS:16.04.201905303", "UbuntuLTS"]
 }
 
 variable "disk-size" {
-  type = "list"
+  type    = "list"
   default = ["10", "20", "40"]
+}
+
+# Resource group name - Change later
+variable "resource-group-name" {
+  type    = "string"
+  default = "myresourcegroup"
+}
+
+variable "location-name" {
+  type    = "string"
+  default = "southcentralus"
 }
