2a61f447cd38f8c84925ea693c7b7e6eff45b665
Kent Tokunaga <KentTokunaga@gmail.com>
2019-06-10 14:10:00 -0500

    Create subnet

--- main.tf
+++ main.tf
@@ -24,3 +24,12 @@ resource "azurerm_virtual_network" "main" {
   location            = "${azurerm_resource_group.main.location}"
   resource_group_name = "${azurerm_resource_group.main.name}"
 }
+
+#Create Subnet
+# TODO: Make Subnet without hardcoding it
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = "${azurerm_resource_group.main.name}"
+  virtual_network_name = "${azurerm_virtual_network.main.name}"
+  address_prefix       = "10.0.0.0/24"
+}
