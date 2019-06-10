43c13938dd55326d2bc3214a820f3968ebe1def7
doradoe1 <doradoe1@outlook.com>
2019-06-10 15:26:23 -0500

    created disk on the vm

--- VM/az-vm.tf
+++ VM/az-vm.tf
@@ -4,15 +4,31 @@ resource "azurerm_virtual_machine" "windowsVM" {
    resource_group_name = "${azurerm_resource_group.windows-rg.name}"
    network_interface_ids = "${var.netword-ii}"
    vm_size = "${var.vm-size}"
+   
+   storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+  
+  storage_os_disk {
+    name              = "${var.disk-name}"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+    disk_size_gb      = "${var.disk-size}"
+    os_type           = "Windows"
+  }
+  
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
    os_profile_windows_config { 
       provision_vm_agent = true
       enable_automatic_upgrades = true
       timezone = "${var.timezone}"
    }
-}
-resource "azurerm_virtual_machine_data_disk_attachment" "vm-disk" {
-  managed_disk_id    = "${azurerm_managed_disk.windows-disk.name}"
-  virtual_machine_id = "${azurerm_virtual_machine.windowsVM.name}"
-  lun                = "10"
-  caching            = "ReadWrite"
 }
