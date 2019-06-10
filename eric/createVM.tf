547ee16200c09b505d72c1bdcea99d68d800daec
EricBAllen <49167218+EricBAllen@users.noreply.github.com>
2019-06-10 15:37:28 -0500

    create vm

--- eric/createVM.tf
+++ eric/createVM.tf
@@ -16,9 +16,18 @@ resource "azurerm_virtual_machine" "windowsVM" {
     resource_group_name = "${azurerm_resource_group.windowsVM.name}" // required
     network_interface_ids = "${azurerm_network_interface.windowsVM.id}" // required 
     vm_size = "${vmSize}" // required 
+
+    storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest" // (optional)
+
+    id = // need this to specify the custom image; changing forces new resource to be created
+  }
     
-    os_profile_windows_config { // required
-        provision_vm_agent = true // (optional) set to true since it automatically defaults to false
+    os_profile_windows_config { # required
+        provision_vm_agent = true # (optional) set to true since it automatically defaults to false
         enable_automatic_upgrades = true // (optional) set to true since it automatically defaults to false
         timezone = "${timeZone}" // (optional)
        // winrm // (optional) windows remote management
