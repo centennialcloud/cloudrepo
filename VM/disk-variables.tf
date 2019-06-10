0bc7e61d5c34526652422baf6a26fbbd7fefe5e7
doradoe1 <doradoe1@outlook.com>
2019-06-10 15:35:13 -0500

    Initiated terraform

--- VM/disk-variables.tf
+++ VM/disk-variables.tf
@@ -1,13 +1,3 @@
-variable "rg-name" {
-    type = "map"
-    description = "the name of the resource group"
-}
-
-variable "location" {
-    type = "map"
-    description = "The location of the resources"
-}
-
 variable "disk-name" {
     type = "map"
     description = "The disk name."
