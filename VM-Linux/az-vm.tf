e061b7aa1df58550e1260227b702f8798edbd84c
doradoe1 <doradoe1@outlook.com>
2019-06-10 16:51:44 -0500

    added network interface

--- VM-Linux/az-vm.tf
+++ VM-Linux/az-vm.tf
@@ -1,8 +1,33 @@
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.networkname}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.linux-rg.name}"
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "${var.subname}-subnet"
+  resource_group_name  = "${azurerm_resource_group.linux-rg.name}"
+  virtual_network_name = "${azurerm_virtual_network.main.name}"
+  address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.netword-ii}"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.linux-rg.name}"
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = "${azurerm_subnet.internal.name}"
+    private_ip_address_allocation = "Dynamic"
+  }
+}
 resource "azurerm_virtual_machine" "LinuxVM" {
    name = "${var.vm-name}-vm"
    location = "${var.location}"
    resource_group_name = "${azurerm_resource_group.linux-rg.name}"
-   network_interface_ids = "${var.netword-ii}"
+   network_interface_ids = "${azurerm_network_interface.main.name}"
    vm_size = "${var.vm-size}"
    
    storage_image_reference {
