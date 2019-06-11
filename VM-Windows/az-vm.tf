6287dc467a1f24f2e90bc941189d604fe56c9593
doradoe1 <doradoe1@outlook.com>
2019-06-11 09:57:05 -0500

    Created a network folder

--- VM-Windows/az-vm.tf
+++ VM-Windows/az-vm.tf
@@ -1,28 +1,3 @@
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.networkname}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.windows-rg.name}"
-}
-
-resource "azurerm_subnet" "internal" {
-  name                 = "${var.subname}-subnet"
-  resource_group_name  = "${azurerm_resource_group.windows-rg.name}"
-  virtual_network_name = "${azurerm_virtual_network.main.name}"
-  address_prefix       = "10.0.2.0/24"
-}
-
-resource "azurerm_network_interface" "main" {
-  name                = "${var.netword-ii}"
-  location            = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.windows-rg.name}"
-
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = "${azurerm_subnet.internal.name}"
-    private_ip_address_allocation = "Dynamic"
-  }
-}
 resource "azurerm_virtual_machine" "windowsVM" {
    name = "${var.vm-name}-vm"
    location = "${var.location}"
--- VM-Windows/network-variables.tf
+++
@@ -1,7 +0,0 @@
-variable "networkname" {
-  description = "The name of the network"
-}
-variable "subname" {
-  description = "The name of the subnetwork"
-}
-
