c88700ba6053932dedea66fd5cb868a624be63b9
doradoe1 <doradoe1@outlook.com>
2019-06-11 10:32:29 -0500

    created terraform init for network

--- Network/net-variables.tf
+++ Network/net-variables.tf
@@ -7,8 +7,11 @@ variable "location" {
 variable "networkname" {
   description = "The name of the network"
 }
-variable "subname" {
-  description = "The name of the subnetwork"
+variable "subpublic" {
+  description = "The name of the public subnetwork"
+}
+variable "subprivate" {
+  description = "The name of the private subnetwork"
 }
 variable "netword-ii" {
     description = "The Network Interface ID's which should be associated with the Virtual Machine."
--- Network/network.tf
+++ Network/network.tf
@@ -1,24 +1,37 @@
-resource "azurerm_virtual_network" "main" {
+resource "azurerm_resource_group" "network-rg" {
+  name = "${var.rg-name}"
+  location = "${var.location}"
+}
+resource "azurerm_virtual_network" "main-network" {
   name                = "${var.networkname}-network"
   address_space       = ["10.0.0.0/16"]
   location            = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.windows-rg.name}"
+  resource_group_name = "${azurerm_resource_group.network-rg.name}"
+  subnet {
+    name = "${var.subpublic}-subnet"
+    address_prefix = "10.0.0.0/18"
+  }
 }
-
 resource "azurerm_subnet" "internal" {
-  name                 = "${var.subname}-subnet"
-  resource_group_name  = "${azurerm_resource_group.windows-rg.name}"
-  virtual_network_name = "${azurerm_virtual_network.main.name}"
-  address_prefix       = "10.0.2.0/24"
+  name                 = "${var.subprivate}-subnet"
+  resource_group_name  = "${azurerm_resource_group.network-rg.name}"
+  virtual_network_name = "${azurerm_virtual_network.main-network.name}"
+  address_prefix       = "10.0.1.0/22"
+   delegation {
+    name = "windowsdelegation"
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
 }
-
 resource "azurerm_network_interface" "main" {
   name                = "${var.netword-ii}"
   location            = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.windows-rg.name}"
+  resource_group_name = "${azurerm_resource_group.network-rg.name}"
 
   ip_configuration {
-    name                          = "testconfiguration1"
+    name                          = "configuration1"
     subnet_id                     = "${azurerm_subnet.internal.name}"
     private_ip_address_allocation = "Dynamic"
   }
