055d490f61c1140dbe81de2de904d9bd47818133
doradoe1 <doradoe1@outlook.com>
2019-06-11 14:41:17 -0500

    Applied NSGs rules to Vnets

--- Network/network.tf
+++ Network/network.tf
@@ -10,21 +10,32 @@ resource "azurerm_virtual_network" "main-network" {
   resource_group_name = "${azurerm_resource_group.network-rg.name}"
 
   subnet {
-    name                      = "${var.networkname}-publicsub"
-    address_prefix            = "10.0.0.0/18"
-
-      service_delegation {
-        name    = "Microsoft.ContainerInstance/containerGroups"
-        actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
-      }
-    }
+    name           = "${var.networkname}-sub"
+    address_prefix = "10.0.0.0/18"
+  }
 }
 
 resource "azurerm_subnet" "private" {
-  name                      = "${var.networkname}-privatesub"
-  resource_group_name       = "${azurerm_resource_group.network-rg.name}"
-  virtual_network_name      = "${azurerm_virtual_network.main-network.name}"
-  address_prefix            = "10.0.1.0/22"
+  name                 = "${var.networkname}-privatesub"
+  resource_group_name  = "${azurerm_resource_group.network-rg.name}"
+  virtual_network_name = "${azurerm_virtual_network.main-network.name}"
+  address_prefix       = "10.0.1.0/22"
+
+  delegation {
+    name = "windowsdelegation"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
+}
+
+resource "azurerm_subnet" "public" {
+  name                 = "${var.networkname}-publicsub"
+  resource_group_name  = "${azurerm_resource_group.network-rg.name}"
+  virtual_network_name = "${azurerm_virtual_network.main-network.name}"
+  address_prefix       = "10.0.1.0/24"
 
   delegation {
     name = "windowsdelegation"
@@ -36,14 +47,14 @@ resource "azurerm_subnet" "private" {
   }
 }
 resource "azurerm_subnet_network_security_group_association" "public" {
-  subnet_id                 = "${var.networkname}-publicsub}"
+  subnet_id                 = "${azurerm_subnet.public.id}"
   network_security_group_id = "${azurerm_network_security_group.public.id}"
 }
+
 resource "azurerm_subnet_network_security_group_association" "private" {
-  subnet_id                 = "${azurerm_subnet.private.name}"
+  subnet_id                 = "${azurerm_subnet.private.id}"
   network_security_group_id = "${azurerm_network_security_group.private.id}"
 }
-
 resource "azurerm_network_interface" "main" {
   name                = "${var.netword-ii}"
   location            = "${var.location}"
@@ -51,7 +62,7 @@ resource "azurerm_network_interface" "main" {
 
   ip_configuration {
     name                          = "configuration1"
-    subnet_id                     = "${azurerm_subnet.private.name}"
+    subnet_id                     = "${azurerm_subnet.private.id}"
     private_ip_address_allocation = "Dynamic"
   }
 }
