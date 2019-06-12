7134d522d92a5da41881c2757af897ec780876bc
doradoe1 <doradoe1@outlook.com>
2019-06-11 22:27:46 -0500

    made lists for outputs on network

--- Network/outputs.tf
+++ Network/outputs.tf
@@ -1,19 +1,23 @@
 output "id" {
-  value = "${azurerm_virtual_network.main-network.id}"
+  value = "${list(azurerm_virtual_network.main-network.id,azurerm_subnet.private.id,azurerm_subnet.public.id)}"
 }
+
 output "name" {
   value = "${azurerm_virtual_network.main-network.name}"
 }
+
 output "address_space" {
   value = "${azurerm_virtual_network.main-network.address_space}"
 }
+
 output "subnet" {
   value = "${azurerm_virtual_network.main-network.subnet}"
 }
+
 output "ip_configurations" {
-  value = "${azurerm_subnet.private.ip_configurations}"
-}
-output "address_prefix" {
-  value = "${azurerm_subnet.private.address_prefix}"
+  value = "${list(azurerm_subnet.private.ip_configurations,azurerm_subnet.public.ip_configurations)}"
 }
 
+output "address_prefix" {
+  value = "${list(azurerm_subnet.private.address_prefix,azurerm_subnet.public.ip_configurations)}"
+}
